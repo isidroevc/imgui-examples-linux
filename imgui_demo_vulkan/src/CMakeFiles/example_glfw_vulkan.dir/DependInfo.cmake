e8b7c7c9fb99d4cd79a2125b666166b79cdb101c
Isidro Vasquez <isidroevc@gmail.com>
2022-08-26 16:16:59 -0500

    First commit

---
+++ imgui_demo_vulkan/src/CMakeFiles/example_glfw_vulkan.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_glfw.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_vulkan.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_vulkan.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/backends/imgui_impl_vulkan.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_demo.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_demo.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_draw.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_draw.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_tables.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_tables.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_widgets.cpp" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/imgui/imgui_widgets.cpp.o.d"
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/src/main.cpp" "CMakeFiles/example_glfw_vulkan.dir/main.cpp.o" "gcc" "CMakeFiles/example_glfw_vulkan.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/elnya/cpp/imgu_examples/imgui_demo_vulkan/src/binary_dir/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
